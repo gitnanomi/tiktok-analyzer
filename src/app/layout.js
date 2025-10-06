769ee3c03296507e18abbf3d15ee7aea9dac99f0
gitnanomi <aimeegogogo123@gmail.com>
2025-10-07 05:09:59 +0800

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,7 +9,7 @@ export default function RootLayout({ children }) {
       <head>
         <script src="https://cdn.tailwindcss.com"></script>
       </head>
-      <body>
+      <body style={{margin: 0, padding: 0}}>
         {children}
       </body>
     </html>
